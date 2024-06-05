8c091e67a0e85f3705c6d730ff82086f6d15e810
Yuval <yuvalmaderer12@gmail.com>
2024-06-05 13:13:21 +0300

    add

--- public/requestProperties/requestProperties.js
+++ public/requestProperties/requestProperties.js
@@ -10,9 +10,27 @@ function addRequestToDom(URL, id) {
   axios.get(`${URL}/${id}`).then((response) => {
     const data = response.data;
     propertiesDiv.innerHTML = `
-    <p>your location: ${data.currentLocation}</p>
-    <p>when you want the dogwolter: ${data.currentTime}</p>
-    <p>duration trip time: ${data.timeDuration}</p>
+    <h3>your location:</h3>
+    <p>${data.currentLocation}</p>
+    <h3>when you want the dogwolter:</h3>
+    <p>${data.currentTime}</p>
+    <h3>duration trip time:</h3>
+    <p> ${data.timeDuration} hours</p>
+    <button onclick="editRequest(this)">edit your request</button>
+    <button onclick="deleteRequest(this)">delete your request</button>
     `;
+  });
+}
+
+function editRequest(svgElement) {
+  const divElem = svgElement.parentNode;
+  const SaveRequestButton = document.createElement("button");
+  SaveRequestButton.textContent = "save your request";
+  divElem.replaceChild(SaveRequestButton, svgElement);
+  const p = divElem.querySelectorAll("p");
+  p.forEach((paragraph) => {
+    const input = document.createElement("input");
+    input.value = paragraph.textContent;
+    divElem.replaceChild(input, paragraph);
   });
 }
