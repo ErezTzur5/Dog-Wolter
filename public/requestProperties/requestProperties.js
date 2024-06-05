8ddfcdb154b8ea81d5ea0a0e54878ceb0cfc9caa
Yuval <yuvalmaderer12@gmail.com>
2024-06-05 12:24:32 +0300

    add

--- public/requestProperties/requestProperties.js
+++ public/requestProperties/requestProperties.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the URL search parameters
+  const dataURL = "http://localhost:8001/requests";
   const params = new URLSearchParams(window.location.search);
-
-  // Get the specific parameter 'id'
-  const userId = params.get("id");
-
-  console.log(userId);
+  const requestId = params.get("id");
+  addRequestToDom(dataURL, requestId);
 });
+
+function addRequestToDom(URL, id) {
+  const propertiesDiv = document.getElementById("requestProperties");
+  axios.get(`${URL}/${id}`).then((response) => {
+    const data = response.data;
+    propertiesDiv.innerHTML = `
+    <p>your location: ${data.currentLocation}</p>
+    <p>when you want the dogwolter: ${data.currentTime}</p>
+    <p>duration trip time: ${data.timeDuration}</p>
+    `;
+  });
+}
